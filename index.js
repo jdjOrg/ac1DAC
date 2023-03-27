254ab25e2bd6f6df3201fddb1e4044e0ed697650
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2023-03-27 20:53:44 -0300

    trabalhando em mudanças e adições
    adicionando telas front para a entidade editora e ajustando algumas partes do front

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const LendoModel = require("./src/models/lendoModel");
 const LerDepoisModel = require("./src/models/lerDepoisModel");
 const EditoraModel = require("./src/models/editoraModel");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 dotenv.config();
 connectToDatabase();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "src/public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src/views"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Renderiza a página principal
 app.get("/", bookController.buscarLivros);
@@ -45,14 +45,19 @@ app.post("/updateLivro/:id", bookController.atualizarLivro);
 // Deleta um livro
 app.get("/deleteLivro/:id", bookController.deletarLivro);
 
+//  -------- A SER DESENVOLVIDO --------------
+app.get("/novaEditora", editoraController.novaEditora);
+app.get("/atualizaLivro/:id", editoraController.inspecionaEditora);
+app.post("/addLivro", editoraController.adicionarEditora);
+app.post("/updateLivro/:id", editoraController.atualizarEditora);
+app.get("/deleteLivro/:id", editoraController.deletarEditora);
+
 // Renderiza a página principal do usuário
 app.get("/user", bookController.livrosUser);
-
 
 // Páginas para adicionar livros as listas
 app.get("/user/addLendo", lendoController.verAddLendo);
 app.get("/user/addLerDepois", lerDepoisController.verAddLerDepois);
-
 
 //Páginas para ver o livro de cada lista
 app.get("/user/verLendo/:id", lendoController.verLendo);
@@ -60,25 +65,27 @@ app.get("/user/verLido/:id", lidoController.verLido);
 app.get("/user/verLerDepois/:id", lerDepoisController.verLerDepois);
 app.get("/user/verCache/:titulo", bookCacheController.verCache);
 
-
 // Métodos para adicionar livros às listas
 app.get("/user/addLivroLendo/:id", lendoController.adicionarLivroLendo);
 app.get("/user/addLivroLido/:titulo", lidoController.adicionarLivroLido);
-app.get("/user/addLivroLerDepois/:id", lerDepoisController.adicionarLivroLerDepois);
-
+app.get(
+	"/user/addLivroLerDepois/:id",
+	lerDepoisController.adicionarLivroLerDepois
+);
 
 // Métodos para deletar livros das listas
 app.post("/user/deleteLivroLendo/:id", lendoController.deletarLivroLendo);
 app.post("/user/deleteLivroLido/:id", lidoController.deletarLivroLido);
-app.post("/user/deleteLivroLerDepois/:id", lerDepoisController.deletarLivroLerDepois);
-
+app.post(
+	"/user/deleteLivroLerDepois/:id",
+	lerDepoisController.deletarLivroLerDepois
+);
 
 // Rotas para atv de DAC
 // Rotas de páginas
 app.get("/dac/editora/home", editoraController.buscarEditoras);
 app.get("/dac/editora/add", editoraController.novaEditora);
 app.post("/dac/editora/single", editoraController.inspecionaEditora);
-
 
 // Rotas de Busca
 app.post("/dac/editora/busca", editoraController.buscaPLocal);
@@ -89,14 +96,13 @@ app.post("/dac/editora/adicionar", editoraController.adicionarEditora);
 app.post("/dac/editora/atualizar/:id", editoraController.atualizarEditora);
 app.post("/dac/editora/deletar/:id", editoraController.deletarEditora);
 
-
 // Rota de testes
 // app.get("/teste/:id", bookController.teste);
 
 app.listen(process.env.EXPRESS_PORT, () =>
-  console.log(
-    `Rodando na URL: http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`
-  )
+	console.log(
+		`Rodando na URL: http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`
+	)
 );
 
 module.exports = app;
--- src/models/editoraModel.js
+++ src/models/editoraModel.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const editoraSchema = new mongoose.Schema({
-  localDeOrigem: {
-    type: String,
-    required: true,
-  },
-  nomeFantasia: {
-    type: String,
-    required: true,
-  },
+	localDeOrigem: {
+		type: String,
+		required: true,
+	},
+	nomeFantasia: {
+		type: String,
+		required: true,
+	},
 });
 
 const EditoraModel = mongoose.model("editora", editoraSchema);
